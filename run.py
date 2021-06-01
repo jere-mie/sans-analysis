2e8950390f2862ccaa14f2cc83d52974f0e6e37a
jere-mie <jere-mie@users.noreply.github.com>
2021-06-01 19:17:20 -0400

    automated db initialization

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from website import app
+from website import app, db
 import os
 if __name__=='__main__':
+    if not os.path.exists("website/site.db"):
+        db.create_all()
     if not os.path.exists("website/static/uploads"):
         os.mkdir("website/static/uploads")
     app.run(debug=True)
