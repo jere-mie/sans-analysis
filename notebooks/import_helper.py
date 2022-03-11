1e5c7c34270fa9fcac8474d5e86b6113ac813fec
jere-mie <jere-mie@users.noreply.github.com>
2022-03-11 00:44:37 -0500

    added notebooks

---
+++ notebooks/import_helper.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir) 
+
+import calculations.fitting as fitting
+import calculations.helpers as helpers
+import calculations.matrices as matrices
+import calculations.workflow as workflow
+
+
